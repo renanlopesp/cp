4b96df9b3311d21cc6dcaef252d96701d5b402ad
renanlopesp <renanlopesp@gmail.com>
2021-10-21 02:51:28 -0300

    update -- name and guild

--- pages/api/power.js
+++ pages/api/power.js
@@ -4,10 +4,10 @@ import * as cheerio from 'cheerio'
 export default function handler(req, res) {
     const info = {
         address: 'https://forum.mir4global.com/rank',
-        ranktype: 1,
-        worldgroupid: 2,
-        worldid: 101,
-        classtype: '',
+        ranktype: req.query.ranktype,
+        worldgroupid: req.query.worldgroupid,
+        worldid: req.query.worldid,
+        classtype: req.query.classtype,
         searchname: req.query.searchname,
     }
 
@@ -17,16 +17,24 @@ export default function handler(req, res) {
         .get(fullUrl)
         .then((response) => {
             const html = response.data
-            const $ = cheerio.load(html)
+            const $ = cheerio.load(html, {
+                xml: {
+                    normalizeWhitespace: true,
+                },
+            })
             const specificPower = []
 
-            $('td.text_right>span', html).each(function () {
-                const title = $(this).text()
-
+            $('.list_article').each(function () {
+                const name = $(this).children('td:nth(1)').text().trim()
+                const guild = $(this).children('td:nth(2)').text().trim()
+                const cp = $(this).children('td:nth(3)').text().trim()
                 specificPower.push({
-                    title,
+                    name,
+                    guild,
+                    cp,
                 })
             })
+
             res.status(200).json(specificPower)
         })
         .catch((err) => console.log(err))
